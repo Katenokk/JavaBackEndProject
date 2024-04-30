108917256ae3f1b1cca7c0852d7ced4453a39209
Katia <katia.obr@gmail.com>
2024-04-30 23:10:35 +0200

    crear nuevo modelo: weight y healthrecor, testeo de mapping entre pet, healthrecord y weight

---
+++ src/main/java/com/pethealth/finalproject/repository/HealthRecordRepository.java
@@ -0,0 +1,9 @@
+package com.pethealth.finalproject.repository;
+
+import com.pethealth.finalproject.model.HealthRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HealthRecordRepository extends JpaRepository<HealthRecord, Long> {
+}
