ad7faf3b10b3f970d8d3fd9e09eaf4944f4185d2
Katia <katia.obr@gmail.com>
2024-05-07 22:02:45 +0200

    no va la clase TokenAuthenticationService

--- src/test/java/com/pethealth/finalproject/TokenAuthenticationService.java
+++ src/test/java/com/pethealth/finalproject/TokenAuthenticationService.java
@@ -1,52 +1,39 @@
 package com.pethealth.finalproject;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Base64;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
 import java.util.Collections;
 import java.util.Date;
+import java.util.stream.Collectors;
 
 public class TokenAuthenticationService {
-    static final long EXPIRATIONTIME = 864_000_000; // 10 days
-    static final String SECRET = "ThisIsASecret";
-    static final String TOKEN_PREFIX = "Bearer";
-    static final String HEADER_STRING = "Authorization";
 
-    public static void addAuthentication(HttpServletResponse res, String username) {
+    public static String createToken(String username, String role) {
+        Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+        // Adding user details and roles to the token
+        String access_token = JWT.create()
+                .withSubject(username)
+                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+                .withIssuer("http://localhost:8080")
+                .withClaim("roles", Collections.singletonList(role))
+                .sign(algorithm);
 
-        String jwt = createToken(username);
-
-        res.addHeader(HEADER_STRING, TOKEN_PREFIX + " " + jwt);
-    }
-
-    public static Authentication getAuthentication(HttpServletRequest request) {
-        String token = request.getHeader(HEADER_STRING);
-        if (token != null) {
-            // parse the token.
-            String user = Jwts.parser()
-                    .setSigningKey(SECRET)
-                    .parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
-                    .getBody()
-                    .getSubject();
-
-            return user != null ?
-                    new UsernamePasswordAuthenticationToken(user, null, Collections.emptyList()) :
-                    null;
-        }
-        return null;
-    }
-
-    public static String createToken(String username) {
-        String jwt = Jwts.builder()
-                .setSubject(username)
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
-                .signWith(SignatureAlgorithm.HS512, SECRET)
-                .compact();
-
-        return jwt;
+        return "Bearer " + access_token;
     }
 }
